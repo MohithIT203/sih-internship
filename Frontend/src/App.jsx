f6e2e065b2f9797091c3a1ffeed59bf25aead7cf
HARJIT M <harjitrm@gmail.com>
2025-09-15 09:25:05 +0530

    updated login and signup page by deleting demo landing page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,10 @@
 import { useState } from "react";
 import Login from "./components/login";import "./App.css";
 import Signin from "./components/signin";
-import Intern from "./components/internship"
 function App() {
   return (
     // <Login/>
     <Signin />
-    // <Intern/>
   );
 }
 
